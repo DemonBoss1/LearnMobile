72747feb66a8fec1f061d55fdfd9a6fb563a1824
DemonBoss1 <121947683+DemonBoss1@users.noreply.github.com>
2023-12-10 16:34:06 +0300

    Add SlidePageAdapter

--- app/src/main/java/com/example/firstapp/DoListActivity.java
+++ app/src/main/java/com/example/firstapp/DoListActivity.java
@@ -3,6 +3,8 @@ package com.example.firstapp;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
 import android.view.View;
@@ -12,6 +14,9 @@ import android.widget.ListView;
 
 public class DoListActivity extends AppCompatActivity {
 
+    public static final int NUM_PAGE = 2;
+    private ViewPager2 viewPager2;
+    private FragmentStateAdapter pagerAdapter;
     Button Button_task_list;
     Button Button_add_task;
 
@@ -21,24 +26,22 @@ public class DoListActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_do_list);
 
+        viewPager2 = findViewById(R.id.fragmentContainerView);
+        pagerAdapter = new SlidePageAdapter(this);
+        viewPager2.setAdapter(pagerAdapter);
+
         Button_task_list = findViewById(R.id.button_task_list);
         Button_add_task = findViewById(R.id.button_add_task);
         Button_task_list.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Fragment fragment = new TaskListFragment();
-                FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
-                fragmentTransaction.replace(R.id.fragmentContainerView, fragment);
-                fragmentTransaction.commit();
+                viewPager2.setCurrentItem(0);
             }
         });
         Button_add_task.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Fragment fragment = new AddTaskFragment();
-                FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
-                fragmentTransaction.replace(R.id.fragmentContainerView, fragment);
-                fragmentTransaction.commit();
+                viewPager2.setCurrentItem(1);
             }
         });
 
---
+++ app/src/main/java/com/example/firstapp/SlidePageAdapter.java
@@ -0,0 +1,29 @@
+package com.example.firstapp;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+public class SlidePageAdapter extends FragmentStateAdapter {
+    public SlidePageAdapter(DoListActivity doListActivity) {
+        super(doListActivity);
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        switch (position){
+            case 0:
+                return new TaskListFragment();
+            case 1:
+                return new AddTaskFragment();
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return DoListActivity.NUM_PAGE;
+    }
+}
