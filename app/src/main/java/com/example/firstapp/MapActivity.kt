53a66b8ca6ad1e80c09c841f2b41a0f793565377
DemonBoss1 <121947683+DemonBoss1@users.noreply.github.com>
2023-12-13 15:18:12 +0300

    Setting up the map

--- app/src/main/java/com/example/firstapp/MapActivity.kt
+++ app/src/main/java/com/example/firstapp/MapActivity.kt
@@ -2,10 +2,38 @@ package com.example.firstapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.yandex.mapkit.Animation
+import com.yandex.mapkit.MapKitFactory
+import com.yandex.mapkit.geometry.Point
+import com.yandex.mapkit.map.CameraPosition
+import com.yandex.mapkit.mapview.MapView
 
 class MapActivity : AppCompatActivity() {
+    private lateinit var mapView: MapView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        MapKitFactory.setApiKey("8e68e3bf-421b-4ae6-ac02-7d71e41c9c36")
+        MapKitFactory.initialize(this)
         setContentView(R.layout.activity_map)
+        mapView = findViewById(R.id.map_view)
+        mapView.map.move(
+                CameraPosition(
+                        Point(56.333939, 43.970847),
+                        18.5f,
+                        -50.0f,
+                        45.0f
+                )
+        )
+    }
+
+    override fun onStart() {
+        mapView.onStart()
+        MapKitFactory.getInstance().onStart()
+        super.onStart()
+    }
+    override fun onStop() {
+        mapView.onStop()
+        MapKitFactory.getInstance().onStop()
+        super.onStop()
     }
 }
