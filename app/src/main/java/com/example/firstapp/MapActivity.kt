637fc97999278e6f452ad1fd771265f0080c3aa7
DemonBoss1 <121947683+DemonBoss1@users.noreply.github.com>
2023-12-13 18:37:19 +0300

    Delete bindingClass

--- app/src/main/java/com/example/firstapp/MapActivity.kt
+++ app/src/main/java/com/example/firstapp/MapActivity.kt
@@ -2,7 +2,6 @@ package com.example.firstapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.firstapp.databinding.ActivityMapBinding
 import com.yandex.mapkit.Animation
 import com.yandex.mapkit.MapKitFactory
 import com.yandex.mapkit.geometry.Point
@@ -10,14 +9,14 @@ import com.yandex.mapkit.map.CameraPosition
 import com.yandex.mapkit.mapview.MapView
 
 class MapActivity : AppCompatActivity() {
-    lateinit var bindingClass : ActivityMapBinding
+    private lateinit var mapView: MapView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         MapKitFactory.setApiKey("8e68e3bf-421b-4ae6-ac02-7d71e41c9c36")
         MapKitFactory.initialize(this)
-        bindingClass = ActivityMapBinding.inflate(layoutInflater)
-        setContentView(bindingClass.root)
-        bindingClass.mapView.map.move(
+        setContentView(R.layout.activity_map)
+        mapView = findViewById(R.id.map_view)
+        mapView.map.move(
                 CameraPosition(
                         Point(56.333939, 43.970847),
                         18.5f,
@@ -28,12 +27,12 @@ class MapActivity : AppCompatActivity() {
     }
 
     override fun onStart() {
-        bindingClass.mapView.onStart()
+        mapView.onStart()
         MapKitFactory.getInstance().onStart()
         super.onStart()
     }
     override fun onStop() {
-        bindingClass.mapView.onStop()
+        mapView.onStop()
         MapKitFactory.getInstance().onStop()
         super.onStop()
     }
